e58ad6725b01dc83f675329c2d374f3d8847ed5d
Gabriel de Oliveira Rocha <97638988+Gabriel-Dev-Oliveira@users.noreply.github.com>
2023-05-28 14:36:56 -0300

    Create positivo_negativo.c

---
+++ positivo_negativo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    
+    printf("Digite um número: ");
+    scanf("%d", &num);
+    
+    if(num % 2 == 0 )
+    {
+        printf("O número %d é par", num);    
+    }
+    else
+    {
+        printf("O número %d é impar", num);
+    }
+}
